d6f933dc1bd61481fb3544a341d3398df58bc263
GundlapalliLokeswarRaju <149657770+GundlapalliLokeswarRaju@users.noreply.github.com>
2025-05-16 11:09:19 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+GITHUB_USER="BigClasses-stack"
+GITHUB_TOKEN="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"
+
 echo "Pulling latest code..."
+git pull https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/myproject.git
 git pull origin main
 
 echo "Building Docker containers..."
