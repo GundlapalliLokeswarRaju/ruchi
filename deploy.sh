717c8c56bc4a959bae043f0e26b866d051c9e984
GundlapalliLokeswarRaju <149657770+GundlapalliLokeswarRaju@users.noreply.github.com>
2025-05-17 12:03:42 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+set -e
 
-set -e  # Exit on any error
 cd /home/ubuntu/myproject
 
+# Clean repo before pull
+git reset --hard HEAD
+git clean -fd
 git config pull.rebase false
 git pull origin main
 
+# Rebuild the app
 docker compose down
 docker compose up -d --build
